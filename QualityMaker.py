544fa92640b91cd7de4902c62a9351355f8219aa
roseg0ld <44655016+roseg0ld@users.noreply.github.com>
2018-11-07 10:10:24 -0800

    Add files via upload

---
+++ QualityMaker.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 14:15:41 2018
+
+@author: Zach
+"""
+
+from __future__ import print_function
+import os
+
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+import numpy as np
+import requests
+
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+
+FILENAME = 'UC Quality'
+SRC_MIMETYPE = 'application/vnd.google-apps.spreadsheet'
+DST_MIMETYPE = 'text/csv'
+
+files = DRIVE.files().list(
+    q='name="%s" and mimeType="%s"' % (FILENAME, SRC_MIMETYPE),
+    orderBy='modifiedTime desc,name').execute().get('files', [])
+
+if files:
+    fn = '%s.csv' % os.path.splitext(files[0]['name'].replace(' ', '_'))[0]
+    print('Exporting "%s" as "%s"... ' % (files[0]['name'], fn), end='')
+    data = DRIVE.files().export(fileId=files[0]['id'], mimeType=DST_MIMETYPE).execute()
+    if data:
+        with open(fn, 'wb') as f:
+            f.write(data)
+        print('DONE')
+    else:
+        print('ERROR (could not download file)')
+else:
+    print('!!! ERROR: File not found')
+
+dt = np.dtype(('U', 128))
+dirName = str(input('What would you like to call the folder?  '))
+
+if not os.path.exists(dirName):
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Created ")
+else:
+    print("Directory " , dirName ,  " already exists")
+
+file = open('UC_Quality.csv')
+
+data = np.genfromtxt(file, delimiter=",",dtype=dt)
+data1 = data
+
+headers=data[[0,1],:]
+data = np.delete(data,(0,1),0) #delete rows
+
+
+titles=data[:,0]
+data = np.delete(data, 0,1) #delete columns
+
+r,c=np.shape(data)
+profs = np.zeros((r,c))
+profs = np.array(profs,dtype=dt)
+
+
+for i in range(r):
+    for j in range(c):
+        if i in (0,4,5,12,13):
+            profs[i,j]=titles[i]
+        else:
+            profs[i,j]=titles[i]+' = '+data[i,j]
+
+
+for k in range(c):
+    varient=data[11,k]
+
+    varient=varient.replace(' ','_')
+    filename = dirName + '/' + data[10,k] + '_' + varient + '_' + data[8,k]+'.inst.cfg'
+    np.savetxt(filename, profs[:,k], newline='\n',fmt='%s')
